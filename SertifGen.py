2fb6ec8f9ea49666064546e1f6fa892259e53073
Mas RJ95 <48236174+maslanangdev@users.noreply.github.com>
2023-05-24 09:28:02 +0700

    skeleton functional python script

--- SertifGen.py
+++ SertifGen.py
@@ -3,41 +3,51 @@ import os
 import qrcode
 import csv
 import base64
-
-data = input("insert csv data : ")
-
-if data == '':
-    data = "sertifPanitia.csv"
-
-with open(data, newline='') as fi:
-    reader = csv.reader(fi)
-    DATA = list(reader)
-
-fi.close
-
-ranged = len(DATA)
-
-with open("basicTemp.svg", 'r') as infile:
-    raw = infile.read()
-
-for i in DATA[1:]:
-        
-    cooked = raw
-    
-    for y in range(len(DATA[0])):
-        toReplace = "{_" + DATA[0][y] + "_}"
-        print(toReplace, end=" => ")
-        cooked = cooked.replace(toReplace, i[y])
-        print(i[y])
-
-    with open("modified.svg", "w") as wrt:
-        wrt.write(cooked)
-    wrt.close()
-    os.system(f'inkscapecom --export-filename="output/{i[0]}.pdf" modified.svg')
-    print("Done : " + i[0])
-
-    print("===================================")
+import inkex
+from inkex.command import inkscape
 
 
 
-infile.close()
+class SertifGen(inkex.base.TempDurMixin, inkex.base.InkscapeExtension):
+    def add_arguments(self, pars):
+        pars.add_argument("--csv-path", type=str, default="")
+        pars.add_argument("--target", type=str, default="")
+
+    def effect(self):
+        data = "sertifPanitia.csv"
+
+        with open(data, newline='') as fi:
+            reader = csv.reader(fi)
+            DATA = list(reader)
+
+        fi.close
+
+        ranged = len(DATA)
+
+        raw = self.document
+
+        for i in DATA[1:]:
+                
+            cooked = raw
+            
+            for y in range(len(DATA[0])):
+                toReplace = "{_" + DATA[0][y] + "_}"
+                print(toReplace, end=" => ")
+                cooked = cooked.replace(toReplace, i[y])
+                print(i[y])
+
+            with open("modified.svg", "w") as wrt:
+                wrt.write(cooked)
+            wrt.close()
+            os.system(f'inkscapecom --export-filename="output/{i[0]}.pdf" modified.svg')
+            print("Done : " + i[0])
+
+            print("===================================")
+
+
+
+        infile.close()
+
+
+if __name__ == '__main__':
+    SertifGen.run()

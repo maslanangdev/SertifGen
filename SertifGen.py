78a4506116fdfea3edf601e005159fb36e83513f
Mas RJ95 <48236174+maslanangdev@users.noreply.github.com>
2023-05-28 23:34:14 +0700

    fully functional !
    all basic functions now working!
    -Export
    -CSV loading

--- SertifGen.py
+++ SertifGen.py
@@ -1,27 +1,34 @@
-from PIL import Image
+
 import os
-# import qrcode
 import csv
-import base64
+
 import inkex
 from inkex.command import inkscape
 
+
+# there's plan to add qrcode support using qrcode package but maybe later
+# import qrcode
+# import base64
+# from PIL import Image
 
 
 class SertifGen(inkex.base.TempDirMixin, inkex.base.InkscapeExtension):
     def add_arguments(self, pars):
         pars.add_argument("--csv-input", type=str, default="")
         pars.add_argument("--target", type=str, default="")
+        pars.add_argument("--export-path", type=str, default="")
 
 
-        # why we're still here
+        # why we're still here (basically useless but needed)
         pars.add_argument("--main", type=str, default="")
         pars.add_argument("--id", type=str, default="")
 
 
     def effect(self):
-        # data = "sertifPanitia.csv"
+
         data = self.options.csv_input
+        exportPath = self.options.export_path
+        targetFormat = self.options.target
 
         with open(data, newline='') as fi:
             reader = csv.reader(fi)
@@ -31,7 +38,6 @@ class SertifGen(inkex.base.TempDirMixin, inkex.base.InkscapeExtension):
 
         raw = self.document
 
-
         modifiedIngredients = os.path.join(self.tempdir, "tempSertif.svg")
 
         for i in DATA[1:]:
@@ -40,22 +46,19 @@ class SertifGen(inkex.base.TempDirMixin, inkex.base.InkscapeExtension):
             
             for y in range(len(DATA[0])):
                 toReplace = "{_" + DATA[0][y] + "_}"
-                print(toReplace, end=" => ")
                 cooked = cooked.replace(toReplace, i[y])
-                with open("ReadyDebug.txt", "a") as wrt:
-                    wrt.write(toReplace)
 
             with open(modifiedIngredients, "w") as wrt:
                 wrt.write(cooked)
             
-
             wrt.close()
-            cli_process = inkscape(modifiedIngredients, actions=f'export-filename:{i[0]}.pdf;export-do;FileClose')
-            
-            self.debug(cli_process)
-            # print("Done : " + i[0])
 
-            # print("===================================")
+            cli_process = inkscape(modifiedIngredients, actions=f'export-filename:{exportPath}/{i[0]}.{targetFormat};export-do;FileClose')
+            
+            if len(cli_process) > 0:
+                self.debug(_("Inkscape returned this messages while exporting, maybe your file(s) is successfully exported but with some warnings : "))
+                self.debug(cli_process)
+
         return True
 
 
